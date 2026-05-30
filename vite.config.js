b43a1a22a7d27fe2e489de96fcec5315385f4621
Tehila <tehila1278@gmail.com>
2026-05-30 23:50:14 +0300

    add to github pg

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Tehila_Dazn_Quiz/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
